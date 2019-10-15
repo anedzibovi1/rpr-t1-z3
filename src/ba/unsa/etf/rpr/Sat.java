9a22fd2a1ec1bc69b5fd1f55ea9dcbfa165afa8c
anedzibovi1 <anedzibovi1@etf.unsa.ba>
2019-10-16 00:38:06 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,50 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Sat {
+    private static int sati,minute,sekunde;
+
+    private Sat(int h, int m, int s) {
+        postavi(h,m,s);
+    }
+
+    private void postavi(int h, int m, int s) {
+        sati=h;
+        minute=m;
+        sekunde=s;
+    }
+
+    private void sljedeci() {
+        sekunde++;
+        if (sekunde==60) { sekunde=0; minute++; }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
+
+    private void prethodni() {
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+    
+    private void pomjeriZa(int pomak) {
+        if (pomak>0) for (int i=0; i<pomak; i++) sljedeci();
+        else for (int i=0; i<-pomak; i++) prethodni();
+    }
+
+    private void ispisi() { System.out.println(sati + " : " + minute +" : "+ sekunde);}
+
+    public static void main(String[] args) {
+        Sat s = new Sat(15,30,45);
+        s.ispisi();
+        s.sljedeci();
+        s.ispisi();
+        s.pomjeriZa(-48);
+        s.ispisi();
+        s.postavi(0,0,0);
+        s.ispisi();
+
+    }
+}
